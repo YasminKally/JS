54ef1569c79b8bd85fa6e582d0a4c91110e60dca
Yasmin Kallyson <yasminkallyson@gmail.com>
2024-02-18 14:04:13 -0300

    Create ambiente.js

---
+++ 13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+
+/* rep com teste no inicio
+while (c <= 8) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/* rep com teste no final */
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 8)
