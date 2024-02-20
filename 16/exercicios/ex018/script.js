7c19189870991fcf1930bc3655bc8268784f3d14
Yasmin Kallyson <yasminkallyson@gmail.com>
2024-02-20 07:34:52 -0300

    exercise completed

--- 16/exercicios/ex018/script.js
+++ 16/exercicios/ex018/script.js
@@ -3,7 +3,7 @@ let lista = document.getElementById('fLista')
 let res = document.getElementById('res')
 let valores = []
 
-function isNum(n) {
+function isNum(n) { //checa se o número está entre 1 e 100
     if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
@@ -11,7 +11,7 @@ function isNum(n) {
     }
 }
 
-function inList(n, l) {
+function inList(n, l) { //checa se o valor está no array
     if (l.indexOf(Number(n))  != -1) {
         return true 
     } else {
@@ -21,12 +21,48 @@ function inList(n, l) {
 
 function adicionar() {
     if (isNum(num.value) && !inList(num.value, valores)) {
-        valores.push(Number(num.value))
+        valores.push(Number(num.value)) //adiciona como number o valor do input
         let opt = document.createElement('option')
-        opt.innerText = `Valor ${valores[valores.indexOf(Number(num.value))]} adicionado`
-        lista.appendChild(opt)
+        opt.text = `Valor ${num.value} adicionado` //valor adicinado como option
+        lista.appendChild(opt) //option adicionado a lista
         console.log(valores)
     } else {
         window.alert('Valor inválido ou já adicionado')
     }
+    num.value = '' //esvazia o input
+    num.focus() //coloca o cursor de texto no input
+    res.innerHTML = ''
+}
+
+function maValor(l) {
+    let maior = Math.max(...l)
+    return maior
+}
+
+function miValor(l) {
+    let menor = Math.min(...l)
+    return menor
+}
+
+function soma(l) {
+    let s = 0
+    for (i = 0; i < l.length; i++) {
+        s += l[i]
+    }
+    return s
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        lista.innerHTML = '' //limpa a lista
+        res.innerHTML += `<br> ${valores.length} valores adicionados <br>`
+        res.innerHTML += `Maior valor adicionado: ${maValor(valores)} <br>`
+        res.innerHTML += `Menor valor adicionado: ${miValor(valores)} <br>`
+        res.innerHTML += `Soma dos valores adicionados: ${soma(valores)} <br>`
+        res.innerHTML += `Média dos valores adicionados: ${soma(valores) / valores.length}`
+        valores = [] //apaga os valores do array
+    }
+    
 }
